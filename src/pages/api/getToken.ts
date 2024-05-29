5b0d8025040dc6087397e9848353dcf25ebcadb4
Ali <52327373+0xAli0@users.noreply.github.com>
2024-05-29 15:41:15 +0300

    Add files via upload

--- src/pages/api/getToken.ts
+++ src/pages/api/getToken.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import cookie from 'cookie';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const sessionToken = req.cookies['next-auth.session-token'];
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const sessionToken = cookies['next-auth.session-token'] || cookies['__Secure-next-auth.session-token'];
+    
     if (!sessionToken) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
