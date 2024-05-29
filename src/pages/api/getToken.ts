9bba399e4c1cac0e276f9e3e6d4e2d24957c55fc
Ali <52327373+0xAli0@users.noreply.github.com>
2024-05-29 15:44:46 +0300

    Add files via upload

--- src/pages/api/getToken.ts
+++ src/pages/api/getToken.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import cookie from 'cookie';
+import { cookies } from 'next/headers';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const cookies = cookie.parse(req.headers.cookie || '');
-    const sessionToken = cookies['next-auth.session-token'] || cookies['__Secure-next-auth.session-token'];
-    
+    const cookieStore = cookies();
+    const sessionToken = cookieStore.get('next-auth.session-token')?.value || cookieStore.get('__Secure-next-auth.session-token')?.value;
+
     if (!sessionToken) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
+    
     res.status(200).json({ token: sessionToken });
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' });
